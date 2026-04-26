5232e2f9d3cdcb9ca9189997ff097be414b12735
Jill Fiore <contact@lumaeris.com>
2026-04-26 16:26:26 +0700

    maybe I have to remove compiled ones?

--- system/ext.sh
+++ system/ext.sh
@@ -8,10 +8,12 @@ for ext in \
 do
     echo "Enabling i18n support for $ext GNOME extension";
     find "/usr/share/gnome-shell/extensions/${ext}/po" -name '*.po' -type f -printf "%P\n" | cut -f1 -d . | while read -r locale; do
+        rm -f /usr/share/locale/${locale}/LC_MESSAGES/${ext}.mo;
         mkdir -p /usr/share/gnome-shell/extensions/${ext}/locale/${locale}/LC_MESSAGES;
         msgfmt /usr/share/gnome-shell/extensions/${ext}/po/${locale}.po -o /usr/share/gnome-shell/extensions/${ext}/locale/${locale}/LC_MESSAGES/${ext}.mo;
     done;
     unset -v locale;
+    rm -f /usr/share/gnome-shell/extensions/$ext/schemas/gschemas.compiled;
     glib-compile-schemas /usr/share/gnome-shell/extensions/$ext/schemas;
 done
 unset -v ext
